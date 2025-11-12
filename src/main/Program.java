dbe1a6177faef32abb8ecb3b2cee2977333965e6
Sajtos Timudzsin Gergő (Szf_N_2024_2026) <sajtos.timudzsin.gergo@diak.szamalk-szalezi.hu>
2025-11-12 14:46:04 +0100

    a gui-ban a jButton-ök át lettek nevezve Lampa-ra, a kódjából a felesleges kommentek el lettek távolítva, és a main metódusa is ki lett törölve, helyett van egy új Program.java osztály ami elindítja (ideiglenesen)

---
+++ src/main/Program.java
@@ -0,0 +1,17 @@
+package main;
+
+import nezet.GUI;
+
+public class Program {
+    
+    public static void main(String[] args) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GUI().setVisible(true);
+            }
+        });
+        
+    }
+    
+}
